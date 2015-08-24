9e163d572cf984390f83a59fccc2262e14bd570f
Hernan Y.Ke <yuriqiao@gmail.com>
2015-08-23 20:48:36 -0400

    Define namespace

---
+++ gq1.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Hernan Y.Ke on 2015/8/23.
+ */
+var com = com || {};
+    com.o2GEEK = com.o2GEEK || {};
+
+
+com.o2GEEK.gQ = function(selector, context){
+
+}
+
+
+com.o2GEEK.gQ.loadJA = function(){
+
+}
