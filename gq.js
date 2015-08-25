6b344ccc96b3a313f7e3be8a6c09230504f7ccde
Hernan Y.Ke <yuriqiao@gmail.com>
2015-08-24 20:55:24 -0400

    Remove

---
+++ gq.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Hernan Y.Ke on 2015/8/24.
+ */
+(function(){
+    //
+})();
--- gq1.js
+++ gq1.js
@@ -1,15 +1,18 @@
 /**
  * Created by Hernan Y.Ke on 2015/8/23.
  */
+
+gQ()
 var com = com || {};
     com.o2GEEK = com.o2GEEK || {};
 
+if(com.o2GEEK.gQ) {
+    com.o2GEEK.gQ = function (selector, context) {
 
-com.o2GEEK.gQ = function(selector, context){
-
-}
+    }
 
 
-com.o2GEEK.gQ.loadJA = function(){
+    com.o2GEEK.gQ.loadJA = function () {
 
+    }
 }
